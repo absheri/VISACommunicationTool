6af5aa863112208513c4b2bad73403621321dc22
HouPoc <hou199488@gmail.com>
2017-02-02 23:15:14 -0800

    to set up icon next step

--- Src/Extention/src/code_ac.py
+++ Src/Extention/src/code_ac.py
@@ -5,16 +5,20 @@ class CodeAC:
 
     def __init__(self, root):
         self.entry = QLineEdit(root)
-        self.list = QListView(root)
+        self.list = QListWidget(root)
+        self.list.IconMode
         self.entry.move(700, 200)
         self.list.move(700,225)
-        self.list.setViewMode(QListView.IconMode)
+        self.list.IconMode
         self.trigger()
 
     def trigger(self):
         self.entry.textChanged[str].connect(self.code_ac)
 
     def code_ac(self, text):
-        ref = open("reference.lib", "r")
+        if len(self.list) != 0:
+            self.list.clear()
+        ref = open("../../Lib/reference.lib", "r")
         for line in ref:
-            if text == line[1]:
+            if text == line[:len(text)]:
+                self.list.addItem(line)
