5f698cd8997e7f4bb407346ebf2431099a5c3e89
HouPoc <hou199488@gmail.com>
2017-02-14 22:29:16 -0800

    RE Construction

--- Src/Extention/src/code_ac.py
+++ Src/Extention/src/code_ac.py
@@ -1,34 +1,23 @@
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+
+
+def get_data(model):
+
+    #model.setStringList(["completion", "data", "goes", "here", "complain"])
+    item = QStandardItem("test")
+    item.setIcon(QIcon('..\..\Src\Img\O.png'))
+    model.insertRow(0,item )
 
 
 class CodeAC:
 
     def __init__(self, root):
         self.entry = QLineEdit(root)
-        self.list = QListWidget(root)
-        self.entry.move(400, 200)
-        self.list.move(400,225)
-        self.trigger()
-
-    def trigger(self):
-        self.entry.textChanged[str].connect(self.code_ac)
-
-    def code_ac(self, text):
-        self.list.clear()
-        index = 0
-        font_size = 22
-        # row line height
-        ref = open("../../Lib/reference.lib", 'r')
-        if text:
-            for line in ref:
-                if text == line[:len(text)]:
-                    lstitem = QListWidgetItem(line[:-1])
-                    lstitem.setIcon(QIcon("..\..\Src\Img\O.png"))
-                    self.list.insertItem(index, lstitem)
-                    index += 1
-            ref.close()
-        if index < 5:
-            self.list.setFixedHeight(font_size * index)
-        else:
-            self.list.setFixedHeight(120)
+        completer = QCompleter()
+        self.entry.setCompleter(completer)
+        model = QStandardItemModel()
+        completer.setModel(model)
+        get_data(model)
+        self.entry.show()
